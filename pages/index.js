779ad7d542adbd730cab6b9b2f88edfd191b95eb
IramAbid <irama4633@gmail.com>
2022-11-12 16:51:10 +0530

    change by iram

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>this is index home opage</h1>
+      <h2>this page is edited by iram</h2>
     </div>
   )
 }
